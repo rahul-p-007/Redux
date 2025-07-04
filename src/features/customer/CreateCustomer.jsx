3a76884a92abb30444045797569ead31eb2cfb79
Rahul <rahulp00rtx@gmail.com>
2025-07-04 13:09:40 +0100

    Completed the action and reducer on the frontend

--- src/features/customer/CreateCustomer.jsx
+++ src/features/customer/CreateCustomer.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createCutomer } from "./customerSlice";
 
 function Customer() {
   const [fullName, setFullName] = useState("");
   const [nationalId, setNationalId] = useState("");
 
-  function handleClick() {}
+  const dispatch = useDispatch(); //for using dispatch function
+
+  function handleClick() {
+    if (!fullName || !nationalId) return;
+    dispatch(createCutomer(fullName, nationalId));
+  }
 
   return (
     <div>
